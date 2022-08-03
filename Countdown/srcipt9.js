03e51c5291a2b05882cc4bf78883c599ab7e5634
vlowther22768 <98420116+vlowther22768@users.noreply.github.com>
2022-08-03 01:15:28 -0400

    Add files via upload

---
+++ Countdown/srcipt9.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.getElementById("bottom").textContent = "This is the end of this document.";
+
+document.getElementsByClassName("main")[1].innerHTML = "here is my paragraph. <br/> <h3>Don't you love it?</h3>";
+
+document.getElementsByTagName("h1")[0].style.fontSize = "40px";
+document.getElementsByTagName("h1")[0].style.color = "blue";
